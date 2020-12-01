7c32c3c9d4517c47f378d5811bc502e88f1b3f5c
Nils Bussas <nils.bussas@gmail.com>
2020-12-01 20:46:51 +0100

    Replace input with text when finish

--- script.js
+++ script.js
@@ -4,9 +4,20 @@ function addTask() {
   newTask = document.createElement("div");
   newTask.setAttribute("class", "task");
   input = document.createElement("input");
+  input.setAttribute("class", "task-input");
+  input.setAttribute("onfocusout", "createTask();");
 
   newTask.appendChild(input);
   taskList.appendChild(newTask);
   input.focus();
   input.select();
+}
+
+function createTask() {
+  taskList = document.getElementById("task-list");
+  task = taskList.lastChild
+  input = task.getElementsByClassName("task-input")[0]
+  task.removeChild(input)
+  txt = document.createTextNode(input.value)
+  task.appendChild(txt)
 }
