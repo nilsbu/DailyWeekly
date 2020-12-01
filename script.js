0aa39d4c354dffbaa7860a9e2ade29d49fb1f097
Nils Bussas <nils.bussas@gmail.com>
2020-12-01 21:29:34 +0100

    Make tasks finishable

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ function addTask() {
 
   newTask = document.createElement("div");
   newTask.setAttribute("class", "task");
+  newTask.setAttribute("id", `task-${taskList.getElementsByClassName("task").length + 1}`);
+
   input = document.createElement("input");
   input.setAttribute("class", "task-input");
   input.setAttribute("onfocusout", "createTask();");
@@ -11,13 +13,20 @@ function addTask() {
   taskList.appendChild(newTask);
   input.focus();
   input.select();
+  newTask.setAttribute("onclick", `finishTask("${newTask.id}");`);
 }
 
 function createTask() {
   taskList = document.getElementById("task-list");
-  task = taskList.lastChild
-  input = task.getElementsByClassName("task-input")[0]
-  task.removeChild(input)
-  txt = document.createTextNode(input.value)
-  task.appendChild(txt)
+  task = taskList.lastChild;
+  input = task.getElementsByClassName("task-input")[0];
+  task.removeChild(input);
+  txt = document.createTextNode(input.value);
+  task.appendChild(txt);
+}
+
+function finishTask(id) {
+  taskList = document.getElementById("task-list");
+  task = document.getElementById(id);
+  task.setAttribute("class", "task-done");
 }
