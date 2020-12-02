8c977e94f215987fdcbc6d3ef336a1c9123bac8c
Nils Bussas <nils.bussas@gmail.com>
2020-12-02 15:05:01 +0100

    Save tasks in .json file

--- script.js
+++ script.js
@@ -23,10 +23,74 @@ function createTask() {
   task.removeChild(input);
   txt = document.createTextNode(input.value);
   task.appendChild(txt);
+  saveList();
 }
 
 function finishTask(id) {
   taskList = document.getElementById("task-list");
   task = document.getElementById(id);
   task.setAttribute("class", "task-done");
+  saveList();
 }
+
+function loadLists(lists) {
+  // TODO: use multiple lists
+  list = lists['lists'][0]['tasks'];
+  taskList = document.getElementById('task-list');
+  while (taskList.firstChild) {
+    taskList.removeChild(taskList.lastChild);
+  }
+
+  for (const task of list) {
+    newTask = document.createElement('div');
+    newTask.setAttribute('class', 'task');
+    newTask.setAttribute('id', `task-${taskList.getElementsByClassName('task').length + 1}`);
+    txt = document.createTextNode(task['txt']);
+
+    if (task['done']) {
+      newTask.setAttribute('class', 'task-done');
+    }
+
+    newTask.appendChild(txt);
+    taskList.appendChild(newTask);
+    newTask.setAttribute('onclick', `finishTask('${newTask.id}');`);
+  }
+}
+
+function saveList() {
+  let tasks = [];
+  taskList = document.getElementById('task-list');
+  for (const child of taskList.children) {
+    task = {'txt': child.textContent, 'done': child.getAttribute('class') === 'task-done'};
+    tasks.push(task);
+  }
+  store.store({'lists': [{'name': 'today', 'tasks': tasks}]});
+}
+
+// Storage
+
+class StorageJson {
+  constructor(onload) {
+    this.onload = onload;
+  }
+
+  store(lists) {
+    localStorage.setItem('lists', JSON.stringify(lists));
+  }
+
+  loadAsync() {
+    let lists = {'lists': [{'name': 'today', 'tasks': []}]};
+    if (localStorage.hasOwnProperty('lists')) {
+      lists = JSON.parse(localStorage.getItem('lists'));
+    }
+    this.onload(lists);
+  }
+}
+
+var store = new StorageJson(loadLists);
+
+// Init
+function init() {
+  store.loadAsync();
+}
+window.addEventListener('load', init)
